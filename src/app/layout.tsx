a73b3bd00337aee42a796eab7e58ecded2bf7057
Rifat islam <merifatislam@gmail.com>
2026-09-24 00:17:26 +0600

    added Navabar component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,17 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/component/shared/Navbar";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+
+const inter = Inter({
   subsets: ["latin"],
+  weight: ['400', '500', '600', '700'],
+  variable: '--font-Inter',
+  display: 'swap',
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+
 
 export const metadata: Metadata = {
   title: "Fit-Log",
@@ -21,9 +22,10 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className="h-full antialiased"
     >
-      <body className="min-h-full flex flex-col">
+      <body className={`min-h-full flex flex-col ${inter.className}  `}>
+        <Navbar/>
         {children}
       </body>
     </html>
---
+++ src/component/shared/Navbar.tsx
@@ -0,0 +1,33 @@
+import logo from "@/app/assets/logo.png";
+import Image from "next/image";
+import Link from "next/link";
+const Navbar = () => {
+  return (
+    <nav className="border-b border-[#202020]">
+      <div className="flex justify-between items-center p-7 ">
+            <div className="flex gap-2 items-center ">
+                <Image src={logo} width={30} height={30} alt="logo"></Image>
+                <h3 className="text-2xl font-semibold">FITLOG</h3>
+            </div>
+            <div className="flex  gap-3 list-none">
+            <Link href={"/"}>
+                <li>Workout</li>
+            </Link>
+            <Link href={"/"}>
+                <li>My Plan</li>
+            </Link>
+            </div>
+            <div className="flex list-none gap-5">
+                <Link href={"/"}>
+                    <li>Plan  <span className=" bg-yellow-400 text-black px-2  py-1 font-semibold text-md rounded-full"> 0</span></li>
+                </Link>
+                <Link href={"/"}>
+                    <li>Saved <span className=" font-inter border border-gray-200 text-white px-2  py-1 font-semibold text-md rounded-full">     0</span></li>
+                </Link>
+            </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
