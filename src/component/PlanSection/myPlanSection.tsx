e8390e4c0342f99d5bd41f4be631a97c6859e0ff
Rifat islam <merifatislam@gmail.com>
2026-09-26 13:38:45 +0600

    added sort features sort by duraction calories, ratings

--- src/component/PlanSection/myPlanSection.tsx
+++ src/component/PlanSection/myPlanSection.tsx
@@ -12,7 +12,8 @@ import { toast } from "react-toastify";
 import EmptyCard from "./emptyCard";
 
 const MyPlanSection = () => {
-  const { todayPlan, setTodayPlan, saveLater, setSavelater } = useContext(PlanContext);
+  const { todayPlan, setTodayPlan, saveLater, setSavelater } =
+    useContext(PlanContext);
 
   //   const minutes = saveLater.map((plan)=> (plan.duration)).reduce((acc, cur)=> acc+cur,0)
   //   const calories = saveLater.map((plan)=> (plan.caloriesBurned)).reduce((acc, cur)=> acc+cur,0)
@@ -20,20 +21,48 @@ const MyPlanSection = () => {
 
   const [active, setActive] = useState("today");
   const handlesButtons = (clicked: "today" | "saved") => {
-    setActive(clicked); 
+    setActive(clicked);
   };
 
-  const handlesTodayPlan =(planCard: IPlanType)=>{
-      const removePlan = todayPlan.filter(tp => tp.id !== planCard.id)
-      setTodayPlan(removePlan)
-      toast.success(`Remove ${planCard.name}`)
+  const handlesTodayPlan = (planCard: IPlanType) => {
+    const removePlan = todayPlan.filter((tp) => tp.id !== planCard.id);
+    setTodayPlan(removePlan);
+    toast.success(`Remove ${planCard.name}`);
+  };
+
+  const handlesSavedPlan = (sevedPlan: IPlanType) => {
+    const removePlan = saveLater.filter((tp) => tp.id !== sevedPlan.id);
+    setSavelater(removePlan);
+    toast.success(`Remove ${sevedPlan.name}`);
+  };
+  const markDone = (data: IPlanType) => {
+    toast.success(`Done ${data.name}`);
+  };
+
+// sort functionality
+
+
+  const [sortBy, setSortBy] =useState<"duration" | "calories" | "rating">("duration")
+  // console.log(sortBy, "sortby")
+   
+  const sortWorkout =(workouts:IPlanType[])=>{
+      const workoutsCopy =[...workouts]
+
+      if(sortBy ==="duration"){
+        workoutsCopy.sort((a,b)=> b.duration - a.duration)
+      }else if(sortBy ==="calories"){
+        workoutsCopy.sort((a,b)=> b.caloriesBurned - a.caloriesBurned)
+      }else if(sortBy ==="rating"){
+        workoutsCopy.sort((a,b)=> b.rating - a.rating)
+      }
+      return workoutsCopy
+      
   }
 
-  const handlesSavedPlan =(sevedPlan: IPlanType)=>{
-    const removePlan = saveLater.filter(tp => tp.id !== sevedPlan.id)
-    setSavelater(removePlan)
-    toast.success(`Remove ${sevedPlan.name}`)
-  }
+  const sortedTodayWorkout = sortWorkout(todayPlan)
+  const sortedSaveWorkout = sortWorkout(saveLater)
+
+
 
   return (
     <div>
@@ -74,34 +103,53 @@ const MyPlanSection = () => {
       </div>
 
 
-
       {/* tab button activity  */}
-      <div className="border border-[#232732] bg-[#151921] rounded-2xl px-2 py-2 w-70 flex gap-1">
-        <button
-          onClick={() => handlesButtons("today")}
-          className={`${active === "today" ? "border bg-[#1F242D] border-[#2B303D]" : " "} rounded-2xl px-4 py-1 cursor-pointer`}
-        >
-          Today’s Plan
-        </button>
-        <button
-          onClick={() => handlesButtons("saved")}
-          className={`${active === "saved" ? "border bg-[#1F242D] border-[#2B303D]" : " "} rounded-2xl px-10 py-1 cursor-pointer`}
-        >
-          Saved
-        </button>
+
+
+      
+      <div className="flex justify-between items-center ">
+        <div className="border border-[#232732] bg-[#151921] rounded-2xl px-2 py-2 w-70 flex gap-1">
+          <button
+            onClick={() => handlesButtons("today")}
+            className={`${active === "today" ? "border bg-[#1F242D] border-[#2B303D]" : " "} rounded-2xl px-4 py-1 cursor-pointer`}
+          >
+            Today’s Plan
+          </button>
+          <button
+            onClick={() => handlesButtons("saved")}
+            className={`${active === "saved" ? "border bg-[#1F242D] border-[#2B303D]" : " "} rounded-2xl px-10 py-1 cursor-pointer`}
+          >
+            Saved
+          </button>
+        </div>
+
+          {/* sort by section */}
+        
+        <div className="flex gap-2">
+          <p>Sort By</p>
+          <select
+            value={sortBy}
+            onChange={(e)=> setSortBy(e.target.value as "duration" | "calories" | "calories")}
+            // defaultValue="Sort by"
+            className="select select-success"
+          >
+            {/* <option disabled={true}>Sort By</option> */}
+            <option value={"duration"}>Duration</option>
+            <option value={"calories"}>Calories</option>
+            <option value={"rating"}>Rating</option>
+          </select>
+        </div>
       </div>
-
-
 
       {/* workout card display */}
 
-        <div className="my-10">
-          <div>
-            {/* today plan */}
-            {active === "today" ? ( 
-              todayPlan.length > 0 ? (
-                <div className=" space-y-5  ">
-                {todayPlan.map((plan, ind) => (
+      <div className="my-10">
+        <div>
+          {/* today plan */}
+          {active === "today" ? (
+            todayPlan.length > 0 ? (
+              <div className=" space-y-5  ">
+                {sortedTodayWorkout.map((plan, ind) => (
                   <div
                     key={ind}
                     className=" grid grid-cols-3 justify-between border bg-[#14171E] border-gray-700 p-4 overflow-hidden rounded-2xl"
@@ -133,69 +181,87 @@ const MyPlanSection = () => {
                         </span>
                       </div>
                     </div>
-                        {/* view details and cancel btn cta */}
+                    {/* view details and cancel btn cta */}
                     <div className=" flex gap-2 items-center justify-end">
-                        <Link href={`/details-page/${plan.id}`}><button className=" border border-gray-500 w-33 h-8 rounded-2xl cursor-pointer">View Details</button></Link>
-                        <button className="flex items-center bg-[#ccff22] text-black w-40 px-2 h-8 rounded-2xl"><IoIosCheckmark  className="text-2xl"/>Mark as Done</button>
-                        <RxCross2 onClick={()=>handlesTodayPlan(plan)} className="text-xl mx-3 cursor-pointer" />
+                      <Link href={`/details-page/${plan.id}`}>
+                        <button className=" border border-gray-500 w-33 h-8 rounded-2xl cursor-pointer">
+                          View Details
+                        </button>
+                      </Link>
+                      <button
+                        onClick={() => markDone(plan)}
+                        className="flex items-center bg-[#ccff22] text-black w-40 px-2 h-8 rounded-2xl"
+                      >
+                        <IoIosCheckmark className="text-2xl" />
+                        Mark as Done
+                      </button>
+                      <RxCross2
+                        onClick={() => handlesTodayPlan(plan)}
+                        className="text-xl mx-3 cursor-pointer"
+                      />
                     </div>
-
                   </div>
                 ))}
               </div>
-              ) : (<EmptyCard />)
             ) : (
+              <EmptyCard />
+            )
+          ) : // saved tabs
 
-              // saved tabs 
+          saveLater.length > 0 ? (
+            <div className="  space-y-5  ">
+              {sortedSaveWorkout.map((plan, ind) => (
+                <div
+                  key={ind}
+                  className=" grid grid-cols-3 justify-between border bg-[#14171E] border-gray-700 p-4 overflow-hidden rounded-2xl"
+                >
+                  <Image
+                    src={plan.image}
+                    width={120}
+                    height={120}
+                    alt={plan.name}
+                    className="w-40 h-25 object-cover rounded-lg "
+                  ></Image>
 
-              (saveLater.length > 0 ? (
-                <div className="  space-y-5  ">
-                {saveLater.map((plan, ind) => (
-                  <div
-                    key={ind}
-                    className=" grid grid-cols-3 justify-between border bg-[#14171E] border-gray-700 p-4 overflow-hidden rounded-2xl"
-                  >
-                    <Image
-                      src={plan.image}
-                      width={120}
-                      height={120}
-                      alt={plan.name}
-                      className="w-40 h-25 object-cover rounded-lg "
-                    ></Image>
-
-                    {/* card name and details  */}
-                    <div className="-ml-57 flex flex-col px-4 justify- space-y-2">
-                      <h2 className="text-2xl font-extrabold">{plan.name}</h2>
-                      <p className="text-gray-400">{plan.equipment}</p>
-                      <div className="flex gap-2">
-                        <span className="flex gap-1 items-center">
-                          <IoIosTimer className="text-[#ccff22] font-bold" />
-                          {plan.duration} Min{" "}
-                        </span>
-                        <span className="flex gap-1 items-center">
-                          <SlEnergy className="text-[#ccff22] font-bold" />
-                          {plan.caloriesBurned} kcal
-                        </span>
-                        <span className="flex gap-1 items-center">
-                          <FaRegStar className="text-[#ccff22] font-bold" />
-                          {plan.rating}
-                        </span>
-                      </div>
+                  {/* card name and details  */}
+                  <div className="-ml-57 flex flex-col px-4 justify- space-y-2">
+                    <h2 className="text-2xl font-extrabold">{plan.name}</h2>
+                    <p className="text-gray-400">{plan.equipment}</p>
+                    <div className="flex gap-2">
+                      <span className="flex gap-1 items-center">
+                        <IoIosTimer className="text-[#ccff22] font-bold" />
+                        {plan.duration} Min{" "}
+                      </span>
+                      <span className="flex gap-1 items-center">
+                        <SlEnergy className="text-[#ccff22] font-bold" />
+                        {plan.caloriesBurned} kcal
+                      </span>
+                      <span className="flex gap-1 items-center">
+                        <FaRegStar className="text-[#ccff22] font-bold" />
+                        {plan.rating}
+                      </span>
                     </div>
-                        {/* view details and cancel btn cta */}
-                    <div className=" flex gap-2 items-center justify-end">
-                        <Link href={`/details-page/${plan.id}`}><button className=" border border-gray-500 w-33 h-8 rounded-2xl cursor-pointer">View Details</button></Link>
-                        <RxCross2 onClick={()=>handlesSavedPlan(plan)} className="text-xl mx-3  cursor-pointer" />
-                    </div>
-
                   </div>
-                ))}
-              </div>
-              ): (<EmptyCard />))
-            )}
-          </div>
+                  {/* view details and cancel btn cta */}
+                  <div className=" flex gap-2 items-center justify-end">
+                    <Link href={`/details-page/${plan.id}`}>
+                      <button className=" border border-gray-500 w-33 h-8 rounded-2xl cursor-pointer">
+                        View Details
+                      </button>
+                    </Link>
+                    <RxCross2
+                      onClick={() => handlesSavedPlan(plan)}
+                      className="text-xl mx-3  cursor-pointer"
+                    />
+                  </div>
+                </div>
+              ))}
+            </div>
+          ) : (
+            <EmptyCard />
+          )}
         </div>
-
+      </div>
     </div>
   );
 };
