a2c86a6263da0f8dc54c39a80174359d9de14aa7
Rifat islam <merifatislam@gmail.com>
2026-09-26 09:49:25 +0600

    change footer logo

--- src/component/shared/Footer.tsx
+++ src/component/shared/Footer.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import footLogo from '@/app/assets/Brand-Logo-Left.png'
+import footLogo from '@/app/assets/logo.png'
 import Image from 'next/image';
 const Footer = () => {
     return (
         <footer className=' flex md:justify-between  md:flex-row  flex-col items-center space-y-2 p-7  mt-5 border-t border-[#202020]'>
             <div className='flex gap-2 '>
-                <Image src={footLogo}  alt='footerLogo' className='w-25'></Image>
-                
+                <Image src={footLogo}  alt='footerLogo' className='w-5 h-auto'></Image>
+                <h4 className='font-bold font-oswald'>FITLOG</h4>
            </div>
            <div>
             <p className='text-gray-300'>© 2026 FitLog — Workout Library. Train hard, log honest.</p>
